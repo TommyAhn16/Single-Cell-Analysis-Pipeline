7bec3a66cf35625c40a1acb8ee15e227ad627c10
Chung Wook <wooksmail316@gmail.com>
2021-07-21 17:40:07 +0900

    minor change

--- Docker_Images/Cell_Ranger/command.sh
+++ Docker_Images/Cell_Ranger/command.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cd /home/ec2-user
 mv /home/ec2-user/cellranger.py /home/ec2-user/vol_mnt/cellranger.py
-/home/ec2-user/aws/install
 aws configure set aws_access_key_id $AWS_KEY
 aws configure set aws_secret_access_key $AWS_SECRET_KEY
 aws configure set region $REGION
