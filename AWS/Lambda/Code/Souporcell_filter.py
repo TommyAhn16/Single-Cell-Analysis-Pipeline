1a236bf01c991b0671fd43961652304ca1abff86
Chung Wook <wooksmail316@gmail.com>
2021-07-26 16:04:26 +0900

    lambda fix

--- AWS/Lambda/Code/Souporcell_filter.py
+++ AWS/Lambda/Code/Souporcell_filter.py
@@ -8,6 +8,9 @@ def lambda_handler(event, context):
     sample_id = ""
     num_clusters = ""
     threads = ""
+    AWS_KEY = ""
+    AWS_SECRET_KEY = ""
+    REGION = ""
 
     for env in env_variables:
         if env["Name"] == "sample_id":
@@ -16,11 +19,21 @@ def lambda_handler(event, context):
             num_clusters = env["Value"]
         elif env["Name"] == "SPC_threads":
             threads = env["Value"]
+        elif env["Name"] == "AWS_KEY":
+            AWS_KEY = env["Value"]
+        elif env["Name"] == "AWS_SECRET_KEY":
+            AWS_SECRET_KEY = env["Value"]
+        elif env["Name"] == "REGION":
+            REGION = env["Value"]
 
     return {
         "sample_id": sample_id,
         "download_bucket": download_bucket,
         "upload_bucket": upload_bucket,
         "threads": threads,
-        "num_clusters": num_clusters
+        "num_clusters": num_clusters,
+        "AWS_KEY": AWS_KEY,
+        "AWS_SECRET_KEY": AWS_SECRET_KEY,
+        "REGION" : REGION
+        
     }
--- AWS/Lambda/Code/vireoSNP_filter.py
+++ AWS/Lambda/Code/vireoSNP_filter.py
@@ -2,21 +2,34 @@ def lambda_handler(event, context):
     # Fixed Variables
     download_bucket = "cellsnp-original-result"
     upload_bucket = "vireosnp-result"
+    
 
     # Variables from input
     env_variables = event["Container"]["Environment"]
     sample_id = ""
     n_donor = ""
+    AWS_KEY = ""
+    AWS_SECRET_KEY = ""
+    REGION = ""
 
     for env in env_variables:
         if env["Name"] == "sample_id":
             sample_id = env["Value"]
         elif env["Name"] == "n_donor":
             n_donor = env["Value"]
+        elif env["Name"] == "AWS_KEY":
+            AWS_KEY = env["Value"]
+        elif env["Name"] == "AWS_SECRET_KEY":
+            AWS_SECRET_KEY = env["Value"]
+        elif env["Name"] == "REGION":
+            REGION = env["Value"]
 
     return {
-        "sample_id": sample_id,
-        "n_donor": n_donor,
-        "download_bucket": download_bucket,
-        "upload_bucket": upload_bucket
+    "sample_id": sample_id,
+    "n_donor": n_donor,
+    "download_bucket":download_bucket,
+    "upload_bucket":upload_bucket,
+    "AWS_KEY": AWS_KEY,
+    "AWS_SECRET_KEY": AWS_SECRET_KEY,
+    "REGION" : REGION
     }
